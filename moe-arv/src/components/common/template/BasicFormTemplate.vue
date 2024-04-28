4b90e8bf1dae7d19f179e42175187cb43f878947
MHN <mohamad.naghavi10@gmail.com>
2024-04-28 19:22:45 +0330

    add basic login form

---
+++ moe-arv/src/components/common/template/BasicFormTemplate.vue
@@ -0,0 +1,29 @@
+<template>
+    <form @submit="() => onSubmit()">
+        <div class="text-center mb-[27px]">
+            <span class="form-title">{{ props.title }}</span>
+        </div>
+        <div class="flex flex-col gap-5">
+            <slot name="body" />
+            <div>
+                <BasicButton variant="primary" block type="submit">Submit</BasicButton>
+                <slot name="bottom"></slot>
+            </div>
+        </div>
+    </form>
+</template>
+<script setup lang="ts">
+import BasicButton from '../input/BasicButton.vue';
+
+const props = defineProps<{
+    onSubmit: Function,
+    title:string
+}>()
+</script>
+<style>
+.form-title{
+    @apply text-warm-grey;
+    font-size: 47px;
+    line-height: 1.19;
+}
+</style>
--- moe-arv/src/components/pages/auth/login/partials/LoginForm.vue
+++ moe-arv/src/components/pages/auth/login/partials/LoginForm.vue
@@ -1,41 +1,67 @@
 <template>
-    <form @submit="submit">
-        <div class="flex flex-col gap-5">
-            <div>
-                <BasicInput type="text" v-model="email" name="email" label="email" :error="errors?.email"/>
-            </div>
-            <div>
-                <BasicInput type="password" v-model="password" name="password" label="password" :error="errors?.password"/>
-            </div>
-            <BasicButton variant="primary" block type="submit">Submit</BasicButton>
-        </div>
-    </form>
+  <BasicFormTemplate :on-submit="submit" title="LOGIN">
+    <template #body>
+      <div>
+        <BasicInput type="text" v-model="email" name="email" label="email" :error="errors?.email" />
+      </div>
+      <div>
+        <BasicInput type="password" v-model="password" name="password" label="password" :error="errors?.password" />
+      </div>
+    </template>
+    <template #bottom>
+      <p class="dont-have-account">Don't have an account? <span>Register Now</span></p>
+    </template>
+  </BasicFormTemplate>
 </template>
 <script setup lang="ts">
 import BasicInput from '@/components/common/input/BasicInput.vue';
 import { useForm, useField } from 'vee-validate';
 import { required } from '@/utils/validations/validations.util.ts';
-import BasicButton from "@/components/common/input/BasicButton.vue";
+import BasicFormTemplate from '@/components/common/template/BasicFormTemplate.vue';
 
 const validationSchema = {
-  email: (value:string) => {
+  email: (value: string) => {
     const req = required(value, "Email")
     if (req !== true) return req
     return true
-  }, 
-  password:  (value:string) => {
+  },
+  password: (value: string) => {
     const req = required(value, "Password")
     if (req !== true) return req
     return true
-  }, 
+  },
 }
 
 const { errors, handleSubmit } = useForm({
   validationSchema
 })
 
-const submit = handleSubmit(()=>{})
+const submit = handleSubmit(() => { })
 
 const { value: email } = useField('email')
 const { value: password } = useField('password')
 </script>
+<style scoped>
+.dont-have-account{
+  @apply text-charcoal-grey;
+  font-size: 16px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.13;
+  letter-spacing: normal;
+  text-align: left;
+  margin: 15px 11px 0 8px;
+}
+
+.dont-have-account span{
+  @apply text-charcoal-grey font-bold;
+  font-size: 16px;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.13;
+  letter-spacing: normal;
+  text-align: left;
+}
+
+</style>
