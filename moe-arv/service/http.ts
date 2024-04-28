abd30724cf0e32acfd29b8ebf359c00be8126c04
MHN <mohamad.naghavi10@gmail.com>
2024-04-28 16:46:46 +0330

    add basic service

---
+++ moe-arv/service/http.ts
@@ -0,0 +1,143 @@
+
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse, InternalAxiosRequestConfig } from "axios";
+
+enum StatusCode {
+  Unauthorized = 401,
+  Forbidden = 403,
+  TooManyRequests = 429,
+  InternalServerError = 500,
+}
+
+
+const injectToken = (config: InternalAxiosRequestConfig): InternalAxiosRequestConfig => {
+  const token = "token";
+  try {
+    if (config && token && token != null && config.headers) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config;
+  } catch (error) {
+    throw new Error(error + "");
+  }
+};
+
+const BASE_URL = "http://base.url"
+
+class Http {
+  private instance: AxiosInstance | null = null;
+
+  private get http(): AxiosInstance {
+    return  this.instance ?? this.initHttp(this.defaultErrorHandler);
+  }
+
+  private configuration: Readonly<Record<string, string | boolean>> | null =
+    null;
+
+  private defaultErrorHandler: boolean = false;
+
+  constructor(defaultErrorHandler: boolean) {
+    this.defaultErrorHandler = defaultErrorHandler
+  }
+
+  
+  initHttp(hasDefaultErrorHandler = true) {
+    const headers : any = {
+      "Content-Type": "application/json"
+      // multipart/form-data
+    }
+    
+    const http = axios.create({
+      baseURL: BASE_URL,
+      timeout:60000,
+      headers
+    });
+
+    // INJECT ACCESS TOKEN
+    http.interceptors.request.use(injectToken, (error) => Promise.reject(error));
+    
+    http.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if(hasDefaultErrorHandler)
+          return this.handleError(error);
+        else return Promise.reject(error)
+      }
+    );
+
+    this.instance = http;
+    return http;
+  }
+
+  public setBaseUrl = (val:string) => {
+    this.http.defaults.baseURL = val;
+  }
+
+  public setHeaders = (val:{[key:string]:any}) => {
+    this.http.defaults.headers =  {...this.http.defaults.headers,...val}
+  }
+
+  request<T = any, R = AxiosResponse<T>>(config: AxiosRequestConfig): Promise<R> {
+    return this.http.request(config);
+  }
+
+  get<T = any, R = AxiosResponse<T>>(url: string, config?: AxiosRequestConfig): Promise<R> {
+    return this.http.get<T, R>(url, config);
+  }
+
+  options<T = any, R = AxiosResponse<T>>(url: string, config?: AxiosRequestConfig): Promise<R> {
+    return this.http.options<T, R>(url, config);
+  }
+
+  post<T = any, R = AxiosResponse<T>>(
+    url: string,
+    data?: any,
+    config?: AxiosRequestConfig
+  ): Promise<R> {
+    return this.http.post<T, R>(url, data, config);
+  }
+
+  put<T = any, R = AxiosResponse<T>>(
+    url: string,
+    data?: T,
+    config?: AxiosRequestConfig
+  ): Promise<R> {
+    return this.http.put<T, R>(url, data, config);
+  }
+
+  delete<T = any, R = AxiosResponse<T>>(url: string, config?: AxiosRequestConfig): Promise<R> {
+    return this.http.delete<T, R>(url, config);
+  }
+
+  // Handle global app errors
+  // We can handle generic app errors depending on the status code
+  private handleError(error:any) {
+    
+    let status:any;
+    if(error.status){
+      status = error.status
+    }
+    else if(error.response && error.response.status){
+      status = error.response.status
+    }
+    switch (status) {
+      case StatusCode.InternalServerError: {
+        // Handle InternalServerError
+        break;
+      }
+      case StatusCode.Unauthorized: {
+        break;
+      }
+      case StatusCode.Forbidden: {
+        break;
+      }
+      default:{
+        break;
+      }
+    }
+
+    return Promise.reject(error);
+  }
+}
+
+export let http = new Http(true);
+
