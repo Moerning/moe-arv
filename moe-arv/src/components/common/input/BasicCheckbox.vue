33e3977b923d8b5bda4e905384b0205cd641ac21
MHN <mohamad.naghavi10@gmail.com>
2024-04-30 20:07:02 +0330

    BasicCheckbox ui

--- moe-arv/src/components/common/input/BasicCheckbox.vue
+++ moe-arv/src/components/common/input/BasicCheckbox.vue
@@ -1,43 +1,65 @@
 <script setup lang="ts">
-    interface InputProps {
-      name:string,
-      label:string,
-    }
+import { ref } from 'vue';
 
-    const model = defineModel()
-    const props = defineProps<InputProps>()
+interface InputProps {
+  name: string,
+  label: string,
+}
+
+const model = defineModel()
+const props = defineProps<InputProps>()
+const theInput = ref()
+
+const clickShadowInput = () => theInput.value.click()
 
 </script>
 
 <template>
-  <input v-model="model" :name="props.name" type="checkbox" />
-  <label class="simple-lable" :for="props.name">{{ props.label }}
+  <label class="simple-lable" :for="props.name">
+    <input ref="theInput" class="hidden" v-model="model" :name="props.name" type="checkbox" />
+    <span @click="clickShadowInput" class="input-shadow" :class="{ 'active-box': model, 'inactive-box': !model }">
+
+    </span>
+    {{ props.label }}
   </label>
 </template>
 <style scoped>
-label{
-  @apply text-[1.14285714286rem] capitalize mb-[8px] ml-2;
+label {
+  @apply text-[1.14285714286rem] capitalize mb-[8px] ml-2 mb-[10px] inline-block;
 }
-.simple-label{
-  @apply text-charcoal-grey ml-2;
+
+.simple-label {
+  @apply text-charcoal-grey;
 }
-.error-label{
+
+.error-label {
   @apply text-danger;
 }
-input{
-    @apply rounded-[4px] bg-[#fff] mb-[10px] px-2;
+
+.input-shadow {
+  @apply rounded-[4px] w-[15px] h-[15px] bg-[#fff] px-2 mr-2 inline-block cursor-pointer;
 }
-.simple-input {
-  @apply border border-[#ddd];
+
+.active-box {
+  @apply bg-water-blue relative;
 }
-.error-input {
-  @apply border border-danger;
+
+.active-box::after{
+  @apply text-white absolute top-[1px] left-[5px];
+  display: inline-block;
+  transform: rotate(45deg);
+  height: 10px;
+  width: 5px;
+  border-bottom: 2px solid white;
+  border-right: 2px solid white;
+  content: "";
 }
-.error-msg {
-  @apply text-danger;
-  margin: 10px 34px 28px 0;
+
+.inactive-box {
+  background-color: #ddd;
 }
-input:focus {
-  @apply border border-white outline-slate-grey;
+
+input {
+  @apply rounded-[4px] bg-[#fff] px-2 mr-2;
 }
 </style>
